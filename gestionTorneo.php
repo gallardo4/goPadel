3235cbdf5eb8ed3e021119a101adc7f66f987d2c
gallardo4 <arnau.gallardo.7e7@itb.cat>
2024-05-27 12:43:26 +0200

    Commit

--- gestionTorneo.php
+++ gestionTorneo.php
@@ -9,16 +9,16 @@ include("components/include/nav.php")
         <form id="crearTorneo" method="POST" class="formularioVerde2" action="./components/api/crearTorneo.proc.php">
         
             <label for="torneo_nom">Nombre del Torneo:</label><br>
-            <input type="text" name="torneo_nom" id="torneo_nom"><br><br>
+            <input class="anyadirComentarioTexto" type="text" name="torneo_nom" id="torneo_nom"><br><br>
 
             <label for="torneo_nivel">Nivel del Torneo:</label><br>
-            <select name="torneo_nivel" required>
+            <select class="anyadirComentario" name="torneo_nivel" required>
                             <option value="c">c</option>
                             <option value="c+">c+</option>
                             <option value="b">b</option>
                             <option value="b+">b+</option>
                             <option value="a">a</option>
-                            <option value="P">Profesional</option>
+                            <option value="P">professional</option>
                         </select><br><br>
 
             <button class="anyadirComentario" type="submit">Crear</button>
