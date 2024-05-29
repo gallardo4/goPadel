5823ee1f1253fcbe6bcf810814f58b1311b22e51
gallardo4 <arnau.gallardo.7e7@itb.cat>
2024-05-29 08:55:59 +0200

    Requisitos de contraseña en pantalla al registrarse

--- register.php
+++ register.php
@@ -2,159 +2,126 @@
 include("components/include/nav.php")
 ?>
 
-    <section class="formularios">
-        <h2>Registrarse</h2> 
+<section class="formularios">
+    <h2>Registrarse</h2>
 
-        <form action="./components/api/afegirUser.proc.php" onsubmit="comprovarContrasenya(event)" method="POST" enctype="multipart/form-data" class="formularioVerde1">
-            <table>
-                <tr>
-                    <td><input class="anyadirComentarioTexto" type="text" name="usu_nom" size="20" placeholder="Nombre" required></td>
-                </tr>
-                <tr>
-                    <td><input class="anyadirComentarioTexto" type="text" name="usu_apellido" size="20" placeholder="Apellido" required></td>
-                </tr>
-                <tr>
-                    <td><input class="anyadirComentarioTexto" type="email" name="usu_mail" size="20" placeholder="Email" required></td>
-                </tr>
-                <tr>
-                    <td><input class="anyadirComentarioTexto" type="tel" name="usu_telf" size="20" placeholder="Teléfono" required></td>
-                </tr>
-                <tr>
-                    <td><input class="anyadirComentarioTexto" type="password" id="usu_contra" name="usu_contra" size="20" required placeholder="Contraseña"></td>
-                </tr>
-                <tr>
-                    <td>
-                        <select class="anyadirComentario" name="usu_genero" required>
-                            <option value="" disabled selected>Género</option>
-                            <option value="M">Hombre</option>
-                            <option value="F">Mujer</option>
-                            <option value="0">Otro</option>
-                        </select><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <select class="anyadirComentario" name="usu_nivel" required>
-                            <option value="" disabled selected>Nivel de Padel</option>
-                            <option value="c">c</option>
-                            <option value="c+">c+</option>
-                            <option value="b">b</option>
-                            <option value="b+">b+</option>
-                            <option value="a">a</option>
-                            <option value="P">Profesional</option>
-                        </select><br>
-                    </td>
-                </tr>
-                <tr>
-                    <td><input class="anyadirComentario" type="file" name="perfilImg" id="perfilImg"><br></td>
-                </tr>
-                <tr>
-                    <td><br><input class="anyadirComentario" type="submit" value="Registrarse"></td>
-                </tr>
-            </table>
-        </form>
-    </section>
+    <form action="./components/api/afegirUser.proc.php" onsubmit="comprovarContrasenya(event)" method="POST" enctype="multipart/form-data" class="formularioVerde1">
+        <table>
+            <tr>
+                <td><input class="anyadirComentarioTexto" type="text" name="usu_nom" size="20" placeholder="Nombre" required></td>
+            </tr>
+            <tr>
+                <td><input class="anyadirComentarioTexto" type="text" name="usu_apellido" size="20" placeholder="Apellido" required></td>
+            </tr>
+            <tr>
+                <td><input class="anyadirComentarioTexto" type="email" name="usu_mail" size="20" placeholder="Email" required></td>
+            </tr>
+            <tr>
+                <td><input class="anyadirComentarioTexto" type="tel" name="usu_telf" size="20" placeholder="Teléfono" required></td>
+            </tr>
+            <tr>
+                <td><input class="anyadirComentarioTexto" type="password" id="usu_contra" name="usu_contra" size="20" required placeholder="Contraseña" oninput="validarContrasena()"></td>
+            </tr>
+            <tr>
+                <td id="passwordRequisitos">
+                    <p id="reqLongitud">La contraseña debe tener entre 8 y 20 caracteres.</p>
+                    <p id="reqMayuscula">Debe contener al menos una letra mayúscula.</p>
+                    <p id="reqMinuscula">Debe contener al menos dos letras minúsculas.</p>
+                    <p id="reqNumero">Debe contener al menos un número.</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <select class="anyadirComentario" name="usu_genero" required>
+                        <option value="" disabled selected>Género</option>
+                        <option value="M">Hombre</option>
+                        <option value="F">Mujer</option>
+                        <option value="0">Otro</option>
+                    </select><br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <select class="anyadirComentario" name="usu_nivel" required>
+                        <option value="" disabled selected>Nivel de Pádel</option>
+                        <option value="c">c</option>
+                        <option value="c+">c+</option>
+                        <option value="b">b</option>
+                        <option value="b+">b+</option>
+                        <option value="a">a</option>
+                        <option value="P">professional</option>
+                    </select><br>
+                </td>
+            </tr>
+            <tr>
+                <td><input class="anyadirComentario" type="file" name="perfilImg" id="perfilImg"><br></td>
+            </tr>
+            <tr>
+                <td><br><input class="anyadirComentario" type="submit" value="Registrarse"><br></td>
+            </tr>
+            <tr>
+                <?php
 
-    <script>
-
-        function comprovarContrasenya(event){
-            let inputPassword = document.getElementById("usu_contra");
-
-            let password = inputPassword.value;
-
-            let longitud = false;
-            let mayusculas = false;
-            let minusculas = false;
-            let numeºros = false;
-
-
-            longitud = comprovarLongitud(password);
-            mayusculas = comprovarMayus(password);
-            minusculas = comprovarMins(password);
-            numeros = comprovarNums(password);
-
-
-            if(!longitud){
-                event.preventDefault();
-                alert("la contraseña es demasiado corta")
-            }
-
-            if(!mayusculas){
-                event.preventDefault();
-                alert("Tiene que tener minimo una mayuscula.")
-            }
-
-            if(!minusculas){
-                event.preventDefault();
-                alert("Tiene que tener minimo 2 minusculas.")
-            }
-
-            if(!numeros){
-                event.preventDefault();
-                alert("Tiene que tener minimo 1 numero.")
-            }
-
-            
-
-        }
-
-        function comprovarLongitud(contraseña){
-            longitudPalabra = contraseña.length;
-
-            if(longitudPalabra<=20 && longitudPalabra>=8){
-                return true;
-            }
-            return false;
-        }
-
-        function comprovarMayus(contraseña){
-            var contador = 0;
-
-            for (let i = 0; i < contraseña.length; i++) {
-                if (contraseña[i]==contraseña[i].toUpperCase() && isNaN(contraseña[i])) {
-                    contador++;
+                if (isset($_REQUEST['msg'])) {
+                    echo "<h5>" . $_REQUEST['msg'] . "</h5>";
                 }
-            }
 
-            console.log(contador);
-            
-            if(contador>0){
-                return true
-            }
+                ?>
+            </tr>
+        </table>
+    </form>
+</section>
 
-            return false
+<script>
+    function validarContrasena() {
+        const password = document.getElementById("usu_contra").value;
+
+        const reqLongitud = document.getElementById("reqLongitud");
+        const reqMayuscula = document.getElementById("reqMayuscula");
+        const reqMinuscula = document.getElementById("reqMinuscula");
+        const reqNumero = document.getElementById("reqNumero");
+
+        if (password.length >= 8 && password.length <= 20) {
+            reqLongitud.style.display = "none";
+        } else {
+            reqLongitud.style.display = "list-item";
         }
 
-        function comprovarMins(contraseña){
-            var contador = 0;
-
-            for (let i = 0; i < contraseña.length; i++) {
-                if (contraseña[i]==contraseña[i].toLowerCase() && isNaN(contraseña[i])) {
-                    contador++;
-                }
-            }
-            console.log(contador);
-
-            if(contador>=2){
-                return true
-            }
-            return false
+        const tieneMayuscula = /[A-Z]/.test(password);
+        if (tieneMayuscula) {
+            reqMayuscula.style.display = "none";
+        } else {
+            reqMayuscula.style.display = "list-item";
         }
 
-        function comprovarNums(contraseña){
-            var num = 0;
-            for (let i = 0; i < contraseña.length; i++) {
-                if (!isNaN(contraseña[i])) {
-                    num++;
-                }
-            }
-            if(num>0){
-                return true
-            }
-            return false    
+        const tieneMinusc = (password.match(/[a-z]/g) || []).length >= 2;
+        if (tieneMinusc) {
+            reqMinuscula.style.display = "none";
+        } else {
+            reqMinuscula.style.display = "list-item";
         }
 
-    </script>
+        const tieneNumero = /[0-9]/.test(password);
+        if (tieneNumero) {
+            reqNumero.style.display = "none";
+        } else {
+            reqNumero.style.display = "list-item";
+        }
+    }
+
+    function comprovarContrasenya(event) {
+        validarContrasena();
+
+        const reqs = document.querySelectorAll("#passwordRequisitos p");
+        for (let req of reqs) {
+            if (req.style.display !== "none") {
+                event.preventDefault();
+                window.location = "./register.php?msg=La contraseña no cumple con todos los requisitos.";
+                break;
+            }
+        }
+    }
+</script>
 
 <?php
 include("components/include/footer.html")
